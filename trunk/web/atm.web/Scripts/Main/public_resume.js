c8bbc48383bdf1dac7c8a606c2eb6f5e591e31a5
kiemieuk <kiemieuk@9eb8cfac-bd3c-4607-a2b4-7e78808243c6>
2015-05-29 03:38:17 +0000

    done on change of schema generator

--- trunk/web/atm.web/Scripts/Main/public_resume.js
+++ trunk/web/atm.web/Scripts/Main/public_resume.js
@@ -2,6 +2,91 @@
 var oTable;
 
 $(function () {
+
+    function loadCountry() {
+        viewModel.countries.removeAll();
+        $.ajax({
+            type: 'POST',
+            url: '/Common/GetCountries',
+            contentType: "application/json; charset=utf-8",
+            success: function (msg) {
+                if (msg.OK) {
+                    var list = ko.mapping.toJS(ko.mapping.fromJSON(msg.list));
+                    $.each(list, function (i, v) {
+                        viewModel.countries.push(v);
+                    });
+                }
+            },
+            error: function (xhr) {
+            }
+        });
+    }
+
+    function loadStates(countryid) {
+        viewModel.states.removeAll();
+        $.ajax({
+            type: 'POST',
+            url: '/Common/GetStates',
+            data: JSON.stringify({ countryid: countryid }),
+            contentType: "application/json; charset=utf-8",
+            success: function (msg) {
+                if (msg.OK) {
+                    var list = ko.mapping.toJS(ko.mapping.fromJSON(msg.list));
+                    $.each(list, function (i, v) {
+                        viewModel.states.push(v);
+                    });
+                }
+            },
+            error: function (xhr) {
+            }
+        });
+    }
+
+    function loadCities(stateid) {
+        viewModel.cities.removeAll();
+        $.ajax({
+            type: 'POST',
+            url: '/Common/GetCities',
+            data: JSON.stringify({ stateid: stateid }),
+            contentType: "application/json; charset=utf-8",
+            success: function (msg) {
+                if (msg.OK) {
+                    var list = ko.mapping.toJS(ko.mapping.fromJSON(msg.list));
+                    $.each(list, function (i, v) {
+                        viewModel.cities.push(v);
+                    });
+                }
+            },
+            error: function (xhr) {
+            }
+        });
+    }
+
+    viewModel = {
+        countries: ko.observableArray([]),
+        states: ko.observableArray([]),
+        cities: ko.observableArray([]),
+        saveprofile:function(d) {
+            $("#notification_dialog .modal-body").html("Adakah anda pasti untuk menyimpan rekod peribadi ini?");
+            $("#notification_dialog").modal({
+                show: 'true',
+                backdrop: 'true',
+                keyboard: 'true'
+            });
+
+            $('#notification_dialog .btn-submit').unbind("click");
+            $('#notification_dialog .btn-submit').click(function() {
+                $('#notification_dialog').modal('hide');
+            });
+
+            $('#notification_dialog .btn-cancel').click(function () {
+                $('#notification_dialog').modal('hide');
+            });
+        }
+    };
+
+    ko.applyBindings(viewModel);
+
     $('#resume a').click(function (e) {
         console.log(e);
         e.preventDefault();
