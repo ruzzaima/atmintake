d50a31553b0d94a5048c730e710c1af1a190e9ce
erymuzuan <erymuzuan@hotmail.com>
2015-11-12 07:38:53 +0800

    New filews

---
+++ tools/atm.migration/Helper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data;
+
+namespace atm.migration
+{
+    static class Helper
+    {
+        public static DateTime? GetNullableDateTime(this IDataReader reader, int ordinal)
+        {
+            var val = reader[ordinal];
+            if (val == DBNull.Value) return null;
+            return (DateTime) val;
+        }
+        public static int? GetNullableInt32(this IDataReader reader, int ordinal)
+        {
+            var val = reader[ordinal];
+            if (val == DBNull.Value) return null;
+            return (int) val;
+        }
+        public static string ToString(this IDataReader reader, int ordinal)
+        {
+            var val = reader[ordinal];
+            if (val == DBNull.Value) return null;
+            return (string) val;
+        }
+    }
+}
